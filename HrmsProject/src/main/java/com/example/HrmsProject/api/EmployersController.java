027375cede1057f41624907b5a571685e1191212
kubilaytuncbas <kubilaytuncbas@icloud.com>
2021-05-27 20:01:35 +0300

    ...

--- HrmsProject/src/main/java/com/example/HrmsProject/api/EmployersController.java
+++ HrmsProject/src/main/java/com/example/HrmsProject/api/EmployersController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.HrmsProject.business.abstracts.EmployerService;
 import com.example.HrmsProject.core.utilities.results.DataResult;
+import com.example.HrmsProject.core.utilities.results.Result;
 import com.example.HrmsProject.entities.concretes.Employer;
+import com.example.HrmsProject.entities.concretes.JobPosition;
 
 @RestController
 @RequestMapping("/api/employers")
@@ -27,6 +31,11 @@ public class EmployersController {
 	public DataResult<List<Employer>> getAll()
 	{
 		return employerService.getAll();			
+	}
+	@PostMapping("/add")
+	public Result add(@RequestBody Employer employer) {
+		return employerService.add(employer);
+		
 	}
 
 }
